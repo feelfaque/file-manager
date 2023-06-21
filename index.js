569ab53bee677ce16aa8abb3e54c50b118ae8dc1
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2023-06-21 15:06:49 +0200

    [fm] added navigation fucntions

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import readline from "node:readline";
 import { homedir } from "node:os";
 import { greet } from "./functions/greet.js";
 import { listFiles } from "./functions/listFiles.js";
+import { displayDirectory } from "./functions/displayDirectory.js";
 
 
 const fileManager = () => {
@@ -12,20 +13,16 @@ const fileManager = () => {
     greet(username);
     const rl = readline.createInterface(process.stdin, process.stdout);
 
-    const displayDirectory = (dir) => {
-        rl.setPrompt(`You're currently in ${dir} \n`);
-    }
-    displayDirectory(homeDirectory);
-    rl.prompt();
+    rl.prompt(displayDirectory(homeDirectory));
     rl.on('line', (command) => {
-        switch(command) {
-            case "ls": {
-                listFiles(currentDirectory);
-            }
-            break;
-            default: {
-                console.log("Invalid input")
-            }
+        if (command === "ls") {
+            listFiles(currentDirectory);
+        } else if (command.startsWith("cd ")) {
+            currentDirectory = `${currentDirectory}/${command.slice(3)}`;
+            displayDirectory(currentDirectory);
+        } else if (command === "up") {
+            currentDirectory = currentDirectory.substring(0, currentDirectory.lastIndexOf('/'));
+            displayDirectory(currentDirectory);
         }
     })
 };
